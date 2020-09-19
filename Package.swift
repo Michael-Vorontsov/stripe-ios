2d7bcfb7e31ada595de78300fd3c10ae701f00fe
Mykhailo Vorontsov <mykhailo.vorontsov@ly.st>
2020-09-19 12:23:35 +0100

    Add cHeaders config

--- Package.swift
+++ Package.swift
@@ -21,7 +21,10 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
             name: "StripeiOS",
-            path: "./Stripe"
+            path: "./Stripe",
+            cSettings: [
+               .headerSearchPath("Stri[e/PublicHeaders"),
+            ]
         ),
 //        .testTarget(
 //            name: "Tests",
