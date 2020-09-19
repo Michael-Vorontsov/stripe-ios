4f88c6a10673770bd0d0c66cb12320f4504c84fa
Michael <mykhailo.vorontsov@infopulse.com.ua>
2020-09-19 21:33:01 +0100

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -6,20 +6,15 @@ import PackageDescription
 let package = Package(
     name: "StripeiOS",
     products: [
-        // Products define the executables and libraries produced by a package, and make them visible to other packages.
         .library(
-            name: "StripeiOS",
+            name: "Stripe",
             type: .static,
             targets: ["StripeiOS"]
         ),
     ],
     dependencies: [
-        // Dependencies declare other packages that this package depends on.
-        // .package(url: /* package url */, from: "1.0.0"),
     ],
     targets: [
-        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
-        // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
             name: "StripeiOS",
             path: "./Stripe",
@@ -36,10 +31,5 @@ let package = Package(
                .headerSearchPath("./")
             ]
         )
-//        .testTarget(
-//            name: "Tests",
-//            dependencies: ["StripeiOS"],
-//            path: "./Tests/Tests"
-//        ),
     ]
 )
